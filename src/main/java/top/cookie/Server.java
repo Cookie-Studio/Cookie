702aeda45390fe3fbfd477ef5e45fc0e43c9cbb2
smart_cmd <3523206925@qq.com>
2020-12-06 15:28:40 +0800

    Default Changelist

---
+++ src/main/java/top/cookie/Server.java
@@ -0,0 +1,11 @@
+package top.cookie;
+
+import com.nukkitx.protocol.bedrock.BedrockServer;
+import java.net.InetSocketAddress;
+
+public class Server {
+    public static void main(String[] args){
+        
+    }
+    private static BedrockServer server = new BedrockServer(new InetSocketAddress("127.0.0.1",19132));
+}
